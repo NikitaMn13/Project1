8cce408b9abf9a858870dfc6ed905e22f5c9b5ca
NikitaMn13 <mykhavko.n.n@gmail.com>
2024-11-05 20:54:52 +0200

    1.0

--- main.js
+++ main.js
@@ -1,1 +1,38 @@
-main
+let cars = ["bmw","tesla","ford"]
+
+let carsList = cars.filter((car) => car.length == 3)
+
+console.log(cars);
+
+let myPromise2 = new Promise ((resolve, reject) => {
+    setTimeout(() =>{
+        let success = false
+        if (success) {
+            resolve("OK!Status = 200")
+        } else {
+            reject("Error")
+        }
+    },2000)
+})
+
+myPromise2.then((message) => {
+    console.log(message);
+}).catch((message) => {
+    console.log(message);
+})
+let myPromise = new Promise ((resolve, reject) => {
+    setTimeout(() =>{
+        let success = false
+        if (success) {
+            resolve("OK!Status = 200")
+        } else {
+            reject("Error")
+        }
+    },3000)
+})
+
+myPromise2.then((message) => {
+    console.log(message);
+}).catch((message) => {
+    console.log(message);
+})
